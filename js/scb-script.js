c14c2eb8261df2b0353d7fb49b92356baa1289d1
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-13 18:33:04 +0100

    FEAT: confronto numeri e fine esercizio base

--- js/scb-script.js
+++ js/scb-script.js
@@ -12,8 +12,38 @@ while (numeri.length < 5) {
   }
 }
 console.log(numeri);
-alert(numeri);
-//---------------------------------------------------------------------------
+alert("CERCA DI MEMORIZZARE QUESTI NUMERI " + numeri);
+//********************
+// SETTO TIMER 30 SECONDI
+//******************** */
+setTimeout(function () {
+  let userNumber = inserisciNumeri();
+  console.log(userNumber);
+  //********************
+  // CONFRONTO I NUMERI
+  //******************** */
+  let ricordati = numeriRicordati(userNumber);
+  console.log(ricordati);
+  //********************
+  // DESCRIZIONE
+  //******************** */
+
+  //FINE FUNZIONE TIME OUT
+}, 2000);
+function numeriRicordati(userNumber) {
+  let numeriRicordati = [];
+  for (let i = 0; i < numeri.length; i++) {
+    for (let u = 0; u < userNumber.length; u++) {
+      if (numeri[i] === userNumber[u]) {
+        numeriRicordati.push(numeri[i]);
+      }
+    }
+  }
+  //   console.log(numeriRicordati);
+  return numeriRicordati;
+}
+
+// ---------------------------------------------------------------------------
 // FUNZIONI
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
@@ -23,4 +53,18 @@ function randomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//---------------------------------------------------------------------------
+// INIZIALIZZO LA FUNZIONE PER RICHIEDERE I NUMERI ALL'UTENTE
+//---------------------------------------------------------------------------
+function inserisciNumeri() {
+  let userNumber = [];
+  for (let i = 0; i < 5; i++) {
+    let remember = parseInt(
+      prompt("INSERISCI UNO ALLA VOLTA I NUMERI CHE HAI VISUALIZZATO POCO FA")
+    );
+    userNumber.push(parseInt(remember));
+  }
+  //   console.log(userNumber);
+  return userNumber;
 }
